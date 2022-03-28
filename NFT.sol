fbc0259feb7fab49c0d30ac032b6802dd42c5330
Diana Poplacenel <diana.poplacenel@berkeley.edu>
2022-03-27 18:32:17 -0700

    first comm

---
+++ NFT.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Decentralized Crowdfunding NFTs", "DCNFT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newItemId;
+    }
+}
+
+/* TO DO 
+1. Create multiple NFT contracts for each "bin" of shares 
+2. Change variable names 
+3. Fix release of money escrow 
+4. Company's address needs to be stored 
+
+*/
